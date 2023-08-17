0e5cadf72416a49fb1584fd8b1ab38ef56fc36ba
Max29xD <marcos291198@gmail.com>
2023-08-17 19:53:07 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-/** @type {import('next').NextConfig} */
-const million = require(million);
+import million from 'million/compiler'
 
-const nextConfig = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+}
 
 const millionConfig = {
   auto: true,
   auto: { rsc: true }
 }
 
-module.exports = million.next(nextConfig, millionConfig)
+export default million.next(nextConfig, millionConfig)
