05f52858c9dcc1b5e9a383f7be60484a1ffe9216
Max29xD <marcos291198@gmail.com>
2023-09-09 00:22:02 -0400

    pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
-const million = require('million/compiler');
+const million = require('million/compiler')
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
 const millionConfig = {
   auto: true,
-  auto: { rsc: true }
+  rsc: true
 }
 
-module.exports = million.next(nextConfig, millionConfig)
+module.exports = withPWA(million.next(nextConfig, millionConfig))
